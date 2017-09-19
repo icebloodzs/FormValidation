ed3713b20c89ef547c28bdd5864b6cfeeb2b797d
icebloodzs <905064863@qq.com>
2017-09-19 09:31:24 +0800

    更改

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,21 @@
-$(function(){
-  $("#btnId").click(function(){
-      $('#myPosters').ajaxSubmit({
-          success: function(responseText){
-              alert(responseText);
-          }
-      });
-  });
-});
+function test(){
+    var form = new FormData(document.getElementById("myPosters"));
+//             var req = new XMLHttpRequest();
+//             req.open("post", "${pageContext.request.contextPath}/public/testupload", false);
+//             req.send(form);
+    $.ajax({
+        url:"#",
+        type:"post",
+        data:form,
+        processData:false,
+        contentType:false,
+        success:function(data){
+            window.clearInterval(timer);
+            alert("成功生成");
+        },
+        error:function(e){
+            alert("错误！！");
+            window.clearInterval(timer);
+        }
+    });        
+}
